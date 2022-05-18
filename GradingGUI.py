c059dfce8e05a480c246802869a0b71204cd922b
mteiska <mika123450@hotmail.com>
2022-05-18 11:39:16 +0300

    Json implementation and quick formatting

--- GradingGUI.py
+++ GradingGUI.py
@@ -1,4 +1,4 @@
-from calendar import c
+
 from lib2to3.pytree import Node
 from turtle import left, right
 import PySimpleGUI as sg
@@ -6,6 +6,9 @@ import os
 import sys
 import pandas as pd
 import csv
+import json
+from io import BytesIO
+from PIL import Image, ImageDraw
 
 #Just for testing how to get files in directory    
 #files_in_directory = os.listdir('C:/Users/mika.teiska@lut.fi/Downloads/OP_HT_Palautus1/HT_Palautus1_palautukset/minimi')
@@ -17,7 +20,32 @@ class Virhetiedot:
     virhe = "",
     vakavuus = 0
     lukumaara = 0
+treedata = sg.TreeData()
 
+
+def initiate_problem_list():
+    lista = []
+    f = open('Problem_list.json', encoding = 'utf-8')
+    virheet = json.load(f)
+
+    category = virheet['violations'][0]['category']
+    
+    treedata.Insert("", 'Toiminnallisuus', 'Toiminnallisuus',
+        [0])
+    for i in virheet['violations']:
+        if category != i['category']:
+            treedata.Insert("", i['category'], i['category'],[0])
+            treedata.Insert(i['category'], i['ID'], i['text'], [0])
+            v = Virhetiedot()
+            category = i['category']
+        else:
+            treedata.Insert(i['category'], i['ID'], i['text'], [0])
+            v = Virhetiedot()
+            v.virhe = i['ID']
+            v.vakavuus = i['error_values']
+            v.lukumaara = i['error_values'].keys()
+            lista.append(v)
+    return lista
 starting_path = sg.popup_get_folder('Anna näytettävä kansio')
 
 if not starting_path:
@@ -27,22 +55,8 @@ if not starting_path:
 
 studentdata = sg.TreeData()
 
-treedata = sg.TreeData()
-### Inserting original problem data to tree ###
-### WIP: change original data coming from JSON
-treedata.Insert("", 'Toiminnallisuus', 'Toiminnallisuus',
-    [0])
-treedata.Insert("Toiminnallisuus", 'Kovakoodattu tuloksia', 'Kovakoodattu tuloksia', [0])
-treedata.Insert("Toiminnallisuus", 'Kovakoodattu tiedoston nimi', 'Kovakoodattu tiedoston nimi', [0])
-treedata.Insert("Toiminnallisuus", 'Kovakoodattu päivämäärä', 'Kovakoodattu päivämäärä', [0])
-treedata.Insert("", 'Tietorakenne', 'Tietorakenne',
-    [0])
-treedata.Insert("Tietorakenne", 'import ei ole päätasolla', 'import ei ole päätasolla', [0])
-treedata.Insert("Tietorakenne", 'import ei tiedoston alussa (ei ennen aliohjelmia ja luokkia)', 
-    'import ei tiedoston alussa (ei ennen aliohjelmia ja luokkia)', [0])
-treedata.Insert("", "Poikkeustenkäsittely","Poikkeustenkäsittely",values=[0])
-treedata.Insert("Poikkeustenkäsittely","Exceptissä väärä virhetyyppi tiedostonkäsittelyssä",
-    "Exceptissä väärä virhetyyppi tiedostonkäsittelyssä",values=[0])
+
+
 
 
 
@@ -52,14 +66,16 @@ layout = [[sg.Text('Opiskelijat')],
                    headings=[],
                    auto_size_columns=True,
                    select_mode=sg.TABLE_SELECT_MODE_EXTENDED,
-                   num_rows=15,
-                   col0_width=7,
+                   num_rows=7,
+                   col0_width=8,
                    key='-PROGRAMS-',
                    show_expanded=False,
                    pad = (2,2),
                    enable_events=True,
                    expand_x=True,
                    expand_y=True,
+                   row_height=30
+                   
                    ),
         sg.Tree(data=studentdata,
                    headings=[],
@@ -78,14 +94,17 @@ layout = [[sg.Text('Opiskelijat')],
 
     [sg.Push(),sg.Text('Laita virheen koodi tähän.')],
    [sg.Tree(data=treedata,
-                   headings=['lukumäärä' ],
-                   auto_size_columns=False,
+                   
+                   headings=['lkm' ],
+                   auto_size_columns=True,
                    select_mode=sg.TABLE_SELECT_MODE_EXTENDED,
-                   num_rows=15,
-                   col0_width=12,
+                   num_rows=12,
+                  
+                   col0_heading = 'Ongelmat koodissa',
+                    col0_width = 25,
                    key='-TREE-',
                    show_expanded=True,
-                   pad = (5,5),
+                   pad = (2,2),
                    enable_events=True,
                    expand_x=True,
                    expand_y=True,
@@ -103,10 +122,10 @@ def add_files_in_folder(parent, dirname):
     for f in files:
         fullname = os.path.join(dirname, f).replace("\\","/")
         if os.path.isdir(fullname):            # if folder, add folder and recurse
-            studentdata.Insert(parent, fullname, f, values=[])
+            studentdata.Insert(parent, fullname, f, values=[0], icon = check[0])
             add_files_in_folder(fullname, fullname)
         else:
-            studentdata.Insert(parent, fullname, f, values=[])
+            studentdata.Insert(parent, fullname, f, values=[0], icon = check[0])
 ### Updating already reviewed programs list ####           
 def update_tree_data(students):
     ready_studentdata = sg.TreeData()
@@ -119,49 +138,37 @@ def key_define(tree):
     item =  tree.Widget.selection()
     return '' if len(item) == 0 else tree.IdToKey[item[0]]
 ###############################################################
+def double_click(tree):
+    item = tree.Widget.selection()[0]
+    key = tree.IdToKey[item]
+    print("AVAIN ON SEURAAVA DOUBLE CLICKISTÄ" ,key)
+    print(studentdata.tree_dict[key].values[-1])
+    index = studentdata.tree_dict[key].values[0]
+    index = (index + 1) % 3
+    studentdata.tree_dict[key].values[-1] = index
+    tree.update(key=key, icon=check[index])
 
+def icon(check):
+    box = (32, 32)
+    background = (255, 255, 255, 0)
+    rectangle = (3, 3, 29, 29)
+    line = ((9, 17), (15, 23), (23, 9))
+    im = Image.new('RGBA', box, background)
+    draw = ImageDraw.Draw(im, 'RGBA')
+    draw.rectangle(rectangle, outline='black', width=3)
+    if check == 1:
+        draw.line(line, fill='black', width=3, joint='curve')
+    elif check == 2:
+        draw.line(line, fill='grey', width=3, joint='curve')
+    with BytesIO() as output:
+        im.save(output, format="PNG")
+        png = output.getvalue()
+    return png
 
-
+check = [icon(0), icon(1), icon(2)]
 
 ### Protyping for error score calculating ###
-def read_csv_and_make_object(file):
-    read_file = pd.read_excel ('//maa1.cc.lut.fi/home/h18439/Desktop/Project code/arviointiohjeet_HT.xlsx')
-    read_file.to_csv ('arviointiohjeet_HT.csv', index = None, header=True, encoding='utf-8')
-    
-    lista = []
- 
-    with open('//maa1.cc.lut.fi/home/h18439/Desktop/Project code/arviointiohjeet_HT.csv',"r", encoding ="utf-8") as csv_file:
-          csv_file.readline()
-          csv_reader = csv.reader(csv_file, delimiter=",")
-          for row in csv_reader:
-            if row == ['', '', '', '', '', '', '', '', '', '']:
-                continue
-            if row:
-                print (row)
-                v = Virhetiedot()
-            
-                if row[1] == "" and row[3] != 'Ei yhtään oikein' and row[3] != 'Kaikista':
-                    v.virhe = lista[len(lista)-1].virhe
-                    v.vakavuus = float(row[2]) 
-                    v.lukumaara = float(row[3])
-                    lista.append(v)
-                
-                else:
-                    if (row[3] == 'Ei yhtään oikein' or row[3] == 'Kaikista'):
-                        row[3] = 100.0
-                   
-                    if '?' in row[2]:
-                        print(row[2])
-                        row[2] = row[2][:-1]
-                    v.virhe = row[1]
-                    v.vakavuus = float(row[2])
-                    v.lukumaara = float(row[3])
-                    lista.append(v)
-           
-    for i in lista:
-        print(i.virhe, i.vakavuus, i.lukumaara)
-    return lista
-    #When we have list we can comapre it to programs dict and get the error score
+
 def mergedicts(dict1, dict2):
     for k in set(dict1.keys()).union(dict2.keys()):
         if k in dict1 and k in dict2:
@@ -171,27 +178,28 @@ def mergedicts(dict1, dict2):
                 # If one of the values is not a dict, you can't continue merging it.
                 # Value from second dict overrides one in first and we move on.
                 yield (k, dict2[k])
-                # Alternatively, replace this with exception raiser to alert you of value conflicts
+               
         elif k in dict1:
             yield (k, dict1[k])
         else:
             yield (k, dict2[k])
-
-            
-        
+       
 def main():
     virhelista = {}
     virheen_lukumaara = 0   
     students = {} 
     virhepisteet = 0
     add_files_in_folder('', starting_path)
-    list = read_csv_and_make_object('//maa1.cc.lut.fi/home/h18439/Desktop/Project code/arviointiohjeet_HT.xlsx')
-    window = sg.Window('Grading tool', layout, resizable=True)
-
+    list = initiate_problem_list()
+    window = sg.Window('Grading tool', layout, resizable=True, finalize = True)
+    
+    
     while True:
         event, values = window.read()
-        studenttree = window['-PROGRAMS-']
+        tree = window['-PROGRAMS-']         # type: sg.Tree
+        tree.bind("<Double-1>", '+DOUBLE')
         if event == 'Exit' or event == sg.WIN_CLOSED:
+            tree.unbind("<Double-1>")
             break
         if event == 'virhe':
             if values['virhe'] in virhelista.keys():
@@ -229,16 +237,22 @@ def main():
                 if virhe.virhe in virhelista.keys():
                     print("Listalla virheiden lukumäärä: ", virhe.lukumaara, "Virheen vakavuus: ", virhe.vakavuus)
                     print(virhelista[values['-TREE-'][0]])
-                    if int(virhe.lukumaara) <= virhelista[values['-TREE-'][0]]:
-                        virhepisteet = virhepisteet + virhe.vakavuus
-                        print("Virheen pisteet ovat :",virhepisteet)
+                    for i in virhe.lukumaara:
+                        if i == 'All' and virhelista[values['-TREE-'][0]]==-1:
+                            virhepisteet = virhepisteet + int(virhe.vakavuus[i])
+                            continue
+                        elif i == 'All':
+                            continue
+                        if int(i) <= virhelista[values['-TREE-'][0]]:
+                            virhepisteet = virhepisteet + int(virhe.vakavuus[i])
+                            print("Virheen pisteet ovat :",virhepisteet)
        ### All occurances are wrong ###
         if event == 'ALL':
-            window['-TREE-'].update(key = values['-TREE-'][0], value = 'Kaikki')
-            virhelista[values['-TREE-'][0]] = 0
+            window['-TREE-'].update(key = values['-TREE-'][0], value = -1)
+            virhelista[values['-TREE-'][0]] = -1 ## -1 is easy repsesentation for all being wrong
         ### If row selected and to dict ###
         if event == '-PROGRAMS-':
-            key = key_define(studenttree)
+            key = key_define(tree)
             
             node = studentdata.tree_dict[key]
             parent_node = studentdata.tree_dict[node.parent]
@@ -255,6 +269,9 @@ def main():
             window['-TREE-'].update(values = treedata)
             if virhelista:  #check if dict exists
                 virhelista = dict.fromkeys(virhelista, 0)
+###Adding checkboxes next to student names for easier marking ###
+        if event.endswith('DOUBLE'):
+            double_click(tree)
                 
         ### Save added rows to other tree structure ###
         if event == 'SAVE':
@@ -268,7 +285,7 @@ def main():
             
 
 
-        #{Opiskelija: {virhekoodi1: lkm1}, Opiskelija2: {Virhekoodi2: lkm2}}
+        #{Opiskelija: {virhekoodi1: lkm1}, Opiskelija2: {Virhekoodi2: lkm2, virhekoodi3 : lkm3}}
 
 
 
